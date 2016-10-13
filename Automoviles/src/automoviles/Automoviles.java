855d8271f2b88e9fa219ed7e9b9670025a90a342
Estudiante <Estudiante@FI454402-47.unal.loc>
2016-10-13 15:47:05 -0500

    bicicleta

---
+++ Automoviles/src/automoviles/Automoviles.java
@@ -0,0 +1,10 @@
+package automoviles;
+
+public class Automoviles {
+
+    public static void main(String[] args) {
+        Autos a= new Autos(1997,2016,"nissan","gris",45000000);
+       AutosLujo Ferrari= new AutosLujo(4, 48, 2017, "ferrari", "negro", 100000000);
+    }
+    
+}
