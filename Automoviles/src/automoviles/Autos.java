32b74578df26910f1dbb87190ebd4eb825cb293f
Estudiante <Estudiante@FI454402-47.unal.loc>
2016-10-13 15:47:34 -0500

    bicicleta

---
+++ Automoviles/src/automoviles/Autos.java
@@ -0,0 +1,18 @@
+
+package automoviles;
+
+
+public class Autos {
+    public int serie, año;
+    public String Marca, color;
+    public double precio;
+
+    public Autos(int serie, int año, String Marca, String color, double precio) {
+        this.serie = serie;
+        this.año = año;
+        this.Marca = Marca;
+        this.color = color;
+        this.precio = precio;
+    }
+
+}
