44d6d15fee9f85a9cc498dd81c1a2ba64ba22383
Estudiante <Estudiante@FI454402-47.unal.loc>
2016-10-25 15:49:39 -0500

    ejercicio gestion de proyectos

--- Gestion De Proyectos/src/gestion/de/proyectos/Company.java
+++ Gestion De Proyectos/src/gestion/de/proyectos/Company.java
@@ -8,25 +8,63 @@ public class Company {
     this.clients=new HashMap<>();
     }
     public boolean addClient(Client cliente){
-    return true;
+        if(!this.clients.containsKey(cliente.getCedula())){
+            this.clients.put(cliente.getCedula(), cliente);
+            return true;
+            }
+            return false;
     }
+    
     public double CallAmountAllMarket(){
-    return 0;
+    double MarketValue=0;
+    for (Client client: this.clients.values()){
+    MarketValue+=client.sumMarketValue();
     }
+    return MarketValue;
+    }
+    
     public double CallAllProfit(){
-    return 0;
+    double ProfitValue=0;
+    for (Client client: this.clients.values()){
+    ProfitValue+=client.SumProfit();
     }
+    return ProfitValue;
+    }
+    
     public ArrayList CallMarketAndProfitByAsset(){
     return null;
     }
+    
     public ArrayList CallMarketAndProfitByAge(){
     return null;
     }
+    
     public Client calClientMaxProfit(){
-       return null;
+       Client maxProfit=null;
+        for (Client cliente : this.clients.values()) {
+            if(maxProfit==null)
+                maxProfit=cliente;
+            else{
+            if(cliente.SumProfit()>maxProfit.SumProfit())
+                maxProfit=cliente;
+            }
+        }
+        return maxProfit;
     }
     
     public ArrayList listClient(){
-      return null;
-    }  
+      ArrayList<Client> Clients = new ArrayList();
+        for (Client cliente : this.clients.values()) 
+            Clients.add(cliente);
+        for (int i = 0; i < Clients.size(); i++) {
+            Client actual=this.clients.get(i);
+            int pos=i;
+            for (int j = i+1; j <Clients.size(); j++) {
+                if(actual.sumMarketValue()>clients.get(j).sumMarketValue()){
+                pos=j;}
+            }
+        Client temp=this.clients.get(pos);
+    } 
+        return Clients;
+    }
 }
