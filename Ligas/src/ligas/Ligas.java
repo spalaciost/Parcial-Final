adf84ef0aa533d4adbbbdea48386c5e9f40ce8f8
Estudiante <Estudiante@FI454402-47.unal.loc>
2016-11-01 15:45:17 -0500

    Ligas

---
+++ Ligas/src/ligas/Ligas.java
@@ -0,0 +1,23 @@
+package ligas;
+
+public class Ligas {
+
+    public static void main(String[] args) {
+        File entrada=new File("bla.txt");
+        
+        Scanner Lectura=new Scanner(entrada);
+        Lectura.useDelimiter(",");
+        while(lectura.hasNext()){
+        name= Lectura.next();
+        lastName= Lectura.next();
+            System.out.println(name);
+            System.out.println(lastName);
+        numTournament= Lectura.nextInt();
+            System.out.println("Tournament");
+            for(i=0, i<numTournament, i++){
+            
+            }
+        }
+    }
+    
+}
